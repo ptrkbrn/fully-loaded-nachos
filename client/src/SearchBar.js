20056a1bdee812daf98fa31eec95bdb57b03d369
Patrick Breen <pressure78@gmail.com>
2021-07-05 01:09:01 -0400

    debugging search

--- client/src/SearchBar.js
+++ client/src/SearchBar.js
@@ -28,7 +28,7 @@ function SearchBar(props) {
       // fetches search results from API
       if (query.length > 2) {
         setSearching(true);
-        fetch(`http://fully-loaded-nachos.herokuapp.com/search?${url.searchParams}`, {
+        fetch(`https://fully-loaded-nachos.herokuapp.com/search?${url.searchParams}`, {
           mode: 'cors',
           headers: {
             'Content-Type': 'application/json',
