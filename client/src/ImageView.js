525e9c4a72ddf10a03a7474452da996bf7be7386
Patrick Breen <pressure78@gmail.com>
2021-07-04 23:26:44 -0400

    update fetch URL for heroku

--- client/src/ImageView.js
+++ client/src/ImageView.js
@@ -28,7 +28,7 @@ function ImageView() {
   // on page load, calls API and gets page data
   useEffect(() => {
     console.log('fetching!');
-    fetch(`http://localhost:9000${window.location.pathname}`, {
+    fetch(`${window.location.pathname}`, {
       mode: 'cors',
       headers: {
         'Access-Control-Allow-Origin': '*',
