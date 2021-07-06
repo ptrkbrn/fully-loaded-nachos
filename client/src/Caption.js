bdc72e6c67220ddfc9ba884e83c0f6ca12cd4c0e
Patrick Breen <pressure78@gmail.com>
2021-07-06 00:00:53 -0400

    remove font-weight rule. not into it.

--- client/src/Caption.js
+++ client/src/Caption.js
@@ -10,7 +10,6 @@ const Caption = styled.p`
     transform: translate(-50%, -50%);
     width: 100%;
     font-family: ${(props) => props.font};
-    font-weight: 600;
     -webkit-text-stroke: 1px black;
     display: ${(props) => props.display};
     @media only screen and (min-width: 830px) {
