470fed90f3d3153c31c28dc80bdee421c57ee09f
Patrick Breen <pressure78@gmail.com>
2021-07-09 12:55:10 -0400

    convert About h1s to h2s

--- client/src/About.js
+++ client/src/About.js
@@ -4,25 +4,25 @@ import { React } from 'react';
 function About() {
   return (
     <section>
-      <h1>Who made this?</h1>
+      <h2>Who made this?</h2>
       <p>
         I did. My name is
         <a href="https//www.ptrkbrn.com">Patrick Breen</a>
         .
       </p>
-      <h1>What&apos;s your stack?</h1>
+      <h2>What&apos;s your stack?</h2>
       <p>Postgres/Node/Express/React. Deployed on Heroku, static image hosting on Backblaze B2.</p>
-      <h1>Can I see your code?</h1>
+      <h2>Can I see your code?</h2>
       <p>
         <a href="https://wwww.https://github.com/ptrkbrn/fully-loaded-nachos">Sure.</a>
       </p>
-      <h1>Are you affiliated with Netflix or Tim Robinson?</h1>
+      <h2>Are you affiliated with Netflix or Tim Robinson?</h2>
       <p>Absolutely not.</p>
-      <h1>
+      <h2>
         I think this app sucks. It&apos;s a bad idea
         and what&apos;s more, you&apos;ve executed it terribly.
         How can I tell you about this personally?
-      </h1>
+      </h2>
       <a href="mailto:hi@ptrkbrn.com">hi@ptrkbrn.com</a>
       <a href="https://www.twitter.com/ptrkbrn">@ptrkbrn</a>
     </section>
